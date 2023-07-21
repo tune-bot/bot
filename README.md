b12ede1a19ecf4a85414564608cfff724ff9d953
cbeimers113 <chrisbeimers113@gmail.com>
2023-07-20 23:12:55 -0400

    Database connection test

---
+++ README.md
@@ -0,0 +1,3 @@
+# tune-bot discord
+
+Discord bot to interface with tunebot and play user playlists in the voice channel. 
--- data/constants.go
+++ data/constants.go
@@ -2,6 +2,5 @@ package data
 
 const (
 	TITLE      = "TuneBot"
-	TOKEN_VAR  = "TUNEBOT_DISCORD_TOKEN"
 	CMD_PREFIX = "?"
 )
